f3e715011aa7b5a0e7f2dd6a3c2e4ab9752e6a67
Tom <tblauwendraat@therp.nl>
2024-12-22 13:43:14 +0100

    [ADD] stock_quant_package_sscc
    This module allows to generate SSCC codes for packages automatically.

---
+++ stock_quant_package_sscc/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Therp BV(<https://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock package SSCC code",
+    "summary": """Automatically select the package number from a given range of SSCC codes""",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-tracking",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Inventory",
+    "depends": ["stock", "sequence_python"],
+    "data": [],
+}
